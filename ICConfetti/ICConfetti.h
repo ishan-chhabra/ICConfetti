e3d02f63b38e8270ee1e88221136c19a2778e070
Ishan Chhabra <ishan.chhabra@hotmail.com>
2019-10-14 18:37:27 +0530

    Initial Commit

---
+++ ICConfetti/ICConfetti.h
@@ -0,0 +1,19 @@
+//
+//  ICConfetti.h
+//  ICConfetti
+//
+//  Created by Ishan Chhabra on 14/10/19.
+//  Copyright © 2019 Ishan Chhabra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ICConfetti.
+FOUNDATION_EXPORT double ICConfettiVersionNumber;
+
+//! Project version string for ICConfetti.
+FOUNDATION_EXPORT const unsigned char ICConfettiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICConfetti/PublicHeader.h>
+
+
